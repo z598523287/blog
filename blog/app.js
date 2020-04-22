2e6ad06446b2629352f33e0fb6e7cdc888057f16
test <598523287@qq.com>
2020-04-22 09:19:06 +0800

    第二次提交

--- blog/app.js
+++ blog/app.js
@@ -2,9 +2,22 @@
 const express = require("express");
 //创建服务器
 const app = express();
+//
+const path = require("path");
+
 //引入home,admin路由
 const home = require("./route/home");
 const admin =require("./route/admin")
+
+//配置使用什么模板引擎
+app.engine("art",require("express-art-template"));
+//配置模板所在目录
+app.set("views",path.join(__dirname,"views"));
+//配置模板默认后缀
+app.set("view engine","art");
+
+//静态资源
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use("/",home);
 app.use("/home",home);
--- blog/route/admin.js
+++ blog/route/admin.js
@@ -3,8 +3,8 @@ const express = require("express");
 //创建模块化路由
 const admin = express.Router();
 
-admin.get("/", (req,res)=> {
-    res.send("欢迎来到管理页面");
+admin.get("/login", (req,res)=> {
+    res.render("");
 })
 //将路由作为模块导出
 module.exports = admin;
